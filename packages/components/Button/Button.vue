f8c5dda156ec2b1c3bc93fcde56455ea761af7da
kiki <18639379883@163.com>
2025-05-28 19:05:21 +0800

    first

---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// Button.vue
+// button.test.tsx
+// types.ts
+// style.css
+// constants.ts
+defineOptions({
+  
+})
+
+</script>
+<template>
+  <button style="color:red;">test button</button>
+</template>
